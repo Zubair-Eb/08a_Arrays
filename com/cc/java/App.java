2fb3e48070ac793b94afdf2ea160ff704dd342bb
Zubair <zubair.yousaf-eberl@gmx.de>
2022-04-26 10:57:27 +0200

    Arrays angelegt

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,8 +1,31 @@
 package com.cc.java;
 
+import java.util.Arrays;
+
 public class App {
     
     public static void main(String[] args) {
+
+
+       int[] intArr= new int[4];
+       output("Arr:" + intArr);
+       output("Array-Länge: " +intArr.length);
+
+       output("1.Element: " + intArr[0]);
+
+       intArr[0] = 1;
+       intArr[1] = 2;
+       intArr[2] = 3;
+       intArr[3] = 4;
+
+       output("1.Element: " + intArr[0]);
+       output("2.Element: " + intArr[1]);
+       output("3.Element: " + intArr[2]);
+       output("4.Element: " + intArr[intArr.length - 1]);
+
+    
+
+        
      
     }
 
