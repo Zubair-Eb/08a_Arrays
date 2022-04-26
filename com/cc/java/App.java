4391b11f15ddb64c145811dda9b61bbf85102cbb
Zubair <zubair.yousaf-eberl@gmx.de>
2022-04-26 11:48:19 +0200

    Arrays mit Forschleife

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -7,21 +7,35 @@ public class App {
     public static void main(String[] args) {
 
 
-       int[] intArr= new int[4];
-       output("Arr:" + intArr);
-       output("Array-Länge: " +intArr.length);
+    //    int[] intArr= new int[4];
+    //    output("Arr:" + intArr);
+    //    output("Array-Länge: " +intArr.length);
 
-       output("1.Element: " + intArr[0]);
+    //    output("1.Element: " + intArr[0]);
 
-       intArr[0] = 1;
-       intArr[1] = 2;
-       intArr[2] = 3;
-       intArr[3] = 4;
+    //    intArr[0] = 1;
+    //    intArr[1] = 2;
+    //    intArr[2] = 3;
+    //    intArr[3] = 4;
 
-       output("1.Element: " + intArr[0]);
-       output("2.Element: " + intArr[1]);
-       output("3.Element: " + intArr[2]);
-       output("4.Element: " + intArr[intArr.length - 1]);
+    //    output("1.Element: " + intArr[0]);
+    //    output("2.Element: " + intArr[1]);
+    //    output("3.Element: " + intArr[2]);
+    //    output("4.Element: " + intArr[intArr.length - 1]);
+
+        String[] strArr= {"Ich", "bin", "Rocky","der", "SuperStar"};
+        // output("1.Element: " + strArr[0]);
+        // output("2.Element: " + strArr[1]);
+        // output("3.Element: " + strArr[2]);
+
+        for (int i = 0; i < strArr.length; i++) {
+
+            output(strArr[i]);
+            
+        }
+
+
+            
 
     
 
